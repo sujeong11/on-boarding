e40a54e073ff287a8f8cc4d2431a0d6568e0a34c
sujeong11 <qalzm7351@gmail.com>
2024-02-08 19:24:14 +0900

    refactor: JpaRepository가 아닌 Repository 상속받도록 수정

--- src/main/java/com/projectlyrics/onboarding/domain/member/repository/MemberRepository.java
+++ src/main/java/com/projectlyrics/onboarding/domain/member/repository/MemberRepository.java
@@ -2,11 +2,13 @@ package com.projectlyrics.onboarding.domain.member.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
 import com.projectlyrics.onboarding.domain.member.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends Repository<Member, Long> {
+
+	Optional<Member> findById(Long memberId);
 
 	boolean existsByLoginId(String loginId);
 
